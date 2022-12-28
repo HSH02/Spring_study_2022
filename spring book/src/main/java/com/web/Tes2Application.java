e353ccf689572f9284959be304bdd03ab5186cd8
한상훈 <83763597+HSH02@users.noreply.github.com>
2022-12-28 11:24:59 +0900

    Add files via upload
    2022 12 28

---
+++ spring book/src/main/java/com/web/Tes2Application.java
@@ -0,0 +1,13 @@
+package com.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tes2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tes2Application.class, args);
+	}
+
+}
